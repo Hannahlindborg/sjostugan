e2aab6b60ba8ac7831b146ff3c72de7c4144cf24
Hannahlindborg <Liha23bo@student.ju.se>
2025-12-11 14:58:10 +0100

    Scrolltrigger animation

--- homepage.js
+++ homepage.js
@@ -11,3 +11,41 @@ window.addEventListener("scroll", () => {
     nav.classList.remove("top-menu-with-background");
   }
 });
+
+// Split scroll effect
+//https://www.youtube.com/watch?v=3ePl0OnmG3Y
+
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+  const verticalScroll = document.querySelector(".vertical-scroll");
+  //const leftColumn = document.querySelector(".left-column");
+  const rightColumn = document.querySelector(".right-column");
+
+  const tl = gsap.timeline({
+    defaults: {
+      ease: "none",
+    },
+    scrollTrigger: {
+      trigger: ".container",
+      start: "top top",
+      end: "+1000",
+      scrub: 2,
+      invalidateOnRefresh: true,
+    },
+  });
+
+  tl.fromTo(
+    rightColumn,
+    { y: () => verticalScroll.offsetHeight - rightColumn.offsetHeight },
+    { y: 0 },
+    "start"
+  );
+
+  scrollTrigger.create({
+    trigger: ".container",
+    start: "top top",
+    end: "",
+    invalidateOnRefresh: true,
+  });
+});
